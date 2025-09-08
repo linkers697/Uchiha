9aa84aab93b103fd3f6f7de9a930c21799a8cbd4
linkers697 <music3fhhgyhfgh@gmail.com>
2025-09-08 22:54:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -84,10 +84,10 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://files.catbox.moe/d4z1dl.jpg"
+    "START_IMG_URL", "https://files.catbox.moe/n9np04.jpg"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://files.catbox.moe/d4z1dl.jpg"
+    "PING_IMG_URL", "https://files.catbox.moe/n9np04.jpg"
 )
 PLAYLIST_IMG_URL = "https://files.catbox.moe/n9np04.jpg"
 STATS_IMG_URL = "https://files.catbox.moe/n9np04.jpg"
